7316aabbc9555dcdaa7178c23e41088ee957f55d
Daniel Hilst <daniel@versatushpc.com.br>
2019-05-22 20:27:47 -0300

    update

--- HN/setup-br0.sh
+++ HN/setup-br0.sh
@@ -3,15 +3,13 @@ ip link add name br0 type bridge
 ip link set br0 up
 ip addr add 192.168.123.1/24 dev br0
 
-# Enable LLDP: https://thenetworkway.wordpress.com/2016/01/04/lldp-traffic-and-linux-bridges/
-echo 16384 > /sys/class/net/br0/bridge/group_fwd_mask
 # Not working 
 # firewall-cmd --zone=external --add-masquerade --permanent
 # firewall-cmd --reload
 #
 systemctl stop firewalld
-iptables -t nat -A POSTROUTING -o wlp0s20u1 -j MASQUERADE
-iptables -A FORWARD -i tap0 -o wlp0s20u1 -j ACCEPT
+iptables -t nat -A POSTROUTING -o wlp0s20u2 -j MASQUERADE
+iptables -A FORWARD -i tap0 -o wlp0s20u2 -j ACCEPT
 sysctl net.ipv4.ip_forward=1
 
 ## on the vm
--- start-all.sh
+++ start-all.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -x
 
 xhost +
+
+sudo fuser -kn tcp 9001
+sudo fuser -kn tcp 9002
+sudo fuser -kn tcp 9003
+
 pushd HN
 sudo ./setup-br0.sh
 sudo ./start-ipmi-sim.sh &
